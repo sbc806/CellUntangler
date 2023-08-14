15b6a93c17c616f79d1de3435087c5d21d68ecf7
sbc806 <sarahc19233@gmail.com>
2023-08-14 19:39:41 +0000

    Changed default curvature of RotatedHyperbolicCopmonent to be -5.0 by changing default radius to be 1/math.sqrt(5) instead of 1.0.

--- mt/mvae/components/component.py
+++ mt/mvae/components/component.py
@@ -20,6 +20,8 @@ from torch import Tensor
 from torch.distributions import Distribution
 import torch.nn.functional as F
 
+import math as math
+
 from ..ops import Manifold, PoincareBall, Hyperboloid, Sphere, StereographicallyProjectedSphere, Euclidean, Universal
 from ..sampling import SamplingProcedure
 
@@ -135,7 +137,7 @@ class RotatedHyperbolicComponent(Component):
                  dim: int,
                  fixed_curvature: bool,
                  sampling_procedure: Type[SamplingProcedure[Q, P]],
-                 radius: float=1.0/torch.sqrt(5)) -> None:
+                 radius: float=1.0/math.sqrt(5)) -> None:
         # Add one to the dimension here on purpose.
         super().__init__(dim + 1, fixed_curvature, sampling_procedure)
         self._nradius = torch.nn.Parameter(torch.tensor(radius), requires_grad=not fixed_curvature)
