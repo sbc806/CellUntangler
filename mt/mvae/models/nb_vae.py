97474e9fe8bb18764e2a1590b419a2d1d975c97d
sbc806 <sarahc19233@gmail.com>
2023-12-29 08:44:49 +0000

    Added initialization to try to make z1 large.

--- mt/mvae/models/nb_vae.py
+++ mt/mvae/models/nb_vae.py
@@ -123,7 +123,10 @@ class NBVAE(ModelVAE):
             self.apply(self._init_weights_he_normal)
         elif config.init == "custom":
             self.components[0].apply(self._init_weights_xavier_uniform)
-          
+        elif config.init == "large_z1":
+            print("Using large z1 initialization")
+            nn.init.normal_(self.components[0].fc_mean.weight, mean=5.0)
+
     def encode(self, x: Tensor, batch: Tensor) -> Tensor:
         x = x.squeeze()
 
