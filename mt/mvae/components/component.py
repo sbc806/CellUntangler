d4bce466af3f55256a88c7b8a99ff59af49a1620
sbc806 <sarahc19233@gmail.com>
2024-03-05 07:56:30 +0000

    Changed default curvature of hyperbolic component to be -5.

--- mt/mvae/components/component.py
+++ mt/mvae/components/component.py
@@ -119,7 +119,7 @@ class HyperbolicComponent(Component):
                  dim: int,
                  fixed_curvature: bool,
                  sampling_procedure: Type[SamplingProcedure[Q, P]],
-                 radius: float = 1.0/math.sqrt(0.1)) -> None:
+                 radius: float = 1.0/math.sqrt(5)) -> None:
         # Add one to the dimension here on purpose.
         super().__init__(dim + 1, fixed_curvature, sampling_procedure)
         self._nradius = torch.nn.Parameter(torch.tensor(radius), requires_grad=not fixed_curvature)
