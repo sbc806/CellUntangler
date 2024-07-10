0238a831d026501affce020759550d6b775ebcfc
sbc806 <sarahc19233@gmail.com>
2024-07-10 23:46:42 +0000

    Changed so that can specify start and end of z1_no_grad and mask_z2.

--- mt/mvae/models/vae.py
+++ mt/mvae/models/vae.py
@@ -229,13 +229,13 @@ class ModelVAE(torch.nn.Module):
 
         if self.config.use_z2_no_grad:
             if epoch_num is not None:
-                if epoch_num >= self.config.start_z2_no_grad:
+                if epoch_num >= self.config.start_z2_no_grad and epoch_num <= self.config.end_z2_no_grad:
                     concat_z = self.create_concat_z(reparametrized[0].z, reparametrized[1].z)
             else:
                 concat_z = self.create_concat_z(reparametrized[0].z, reparametrized[1].z)
         if self.config.mask_z2:
             if epoch_num is not None:
-                if epoch_num <= self.config.end_mask_z2:
+                if epoch_num <= self.config.end_mask_z2 and epoch_num >= self.config.start_mask_z2:
                     concat_z = concat_z * self.mask_z1
 
         if self.config.print_concat_z:
