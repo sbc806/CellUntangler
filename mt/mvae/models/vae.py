4a584accc0efd6b07bc7f16cff83e3de3bd538ab
sbc806 <sarahc19233@gmail.com>
2024-07-10 02:59:13 +0000

    Changed so that kl-divergence of z1 does has own multiplication and do not have to run z2_no_grad for all epochs.

--- mt/mvae/models/vae.py
+++ mt/mvae/models/vae.py
@@ -233,7 +233,7 @@ class ModelVAE(torch.nn.Module):
                     concat_z = self.create_concat_z(reparametrized[0].z, reparametrized[1].z)
             else:
                 concat_z = self.create_concat_z(reparametrized[0].z, reparametrized[1].z)
-        elif self.config.mask_z2:
+        if self.config.mask_z2:
             if epoch_num <= self.config.end_mask_z2:
                 concat_z = concat_z * self.mask_z1
 
