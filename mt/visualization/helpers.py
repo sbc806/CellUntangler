8f27e6aa91d2338c54142083d4ea178f6297f082
sbc806 <sarahc19233@gmail.com>
2024-11-28 15:03:26 +0000

    Deleted some files and added helper functions.

---
+++ mt/visualization/helpers.py
@@ -0,0 +1,98 @@
+import math as math
+import numpy as np
+
+def split_embeddings(model_name, embeddings):
+  component_embeddings = {}
+  components = model_name.split(",")
+  components = [c.strip() for c in components]
+  start = 0
+  for i, c in enumerate(components):
+    latent_space = c[0]
+    dim = int(c[1:])
+    if latent_space == "s":
+      dim = dim + 1
+    elif latent_space == "h":
+      dim = dim + 1
+    elif latent_space == "r":
+      dim = dim + 1
+    print(c, dim)
+    component_embeddings[f"component{i+1}_{c}"]=embeddings[:,start:start+dim]
+    start = start + dim
+  return component_embeddings
+
+
+def get_mvae_pseudotime(embeddings, curvature, origin, rotation_angle, flip=False, new_origin=None):
+    poincare_coordinates=lorentz_to_poincare(embeddings, curvature)
+    # translated_poincare_coordinates=poincare_translation(np.array(origin),poincare_coordinates,curvature)
+    # rotated_poincare_coordinates=rotate_coordinates(poincare_coordinates,
+                                                    # math.radians(rotation_angle),
+                                                    # origin,
+                                                    # 1)
+    if new_origin:
+        poincare_coordinates = poincare_translation(np.array(new_origin), poincare_coordinates, curvature)
+    mvae_pseudotime = get_degrees_angles(poincare_coordinates, origin)
+    rotated_mvae_pseudotime = mvae_pseudotime + rotation_angle
+    greater_than_360 = rotated_mvae_pseudotime > 360
+    less_than_0 = rotated_mvae_pseudotime < 0
+    rotated_mvae_pseudotime[greater_than_360] = rotated_mvae_pseudotime[greater_than_360] - 360
+    rotated_mvae_pseudotime[less_than_0] = rotated_mvae_pseudotime[less_than_0] + 360
+    if flip:
+      rotated_mvae_pseudotime=360-rotated_mvae_pseudotime
+    # else:
+      # mvae_pseudotime=get_degrees_angles(rotated_poincare_coordinates,origin)
+    # return rotated_poincare_coordinates, mvae_pseudotime
+    return poincare_coordinates, rotated_mvae_pseudotime
+
+
+def poincare_translation(v, x, curvature):
+    """
+    Computes the translation of x when we move v to the center.
+    Hence, the translation of u with -u should be the origin.
+    """
+    abs_curvature = abs(curvature)
+    xsq = (x ** 2).sum(axis=1)
+    vsq = (v ** 2).sum()
+    xv = (x * v).sum(axis=1)
+    a = np.matmul((abs_curvature * xsq + 2 * abs_curvature * xv + 1).reshape(-1, 1),
+                  v.reshape(1, -1)) + (1 - abs_curvature * vsq) * x
+    b = (abs_curvature**2) * xsq * vsq + 2 * abs_curvature * xv + 1
+
+    return np.dot(np.diag(1. / b), a)
+
+
+def get_degrees_angles(coordinates, origin=(0, 0)):
+  x_vals = coordinates[:, 0]-origin[0]
+  y_vals = coordinates[:, 1]-origin[1]
+  print(x_vals)
+  print(y_vals)
+  angles = np.zeros(coordinates.shape[0])
+  for i in range(coordinates.shape[0]):
+    angles[i] = math.atan2(y_vals[i], x_vals[i])
+    angles[i] = math.degrees(angles[i])
+    if angles[i] < 0:
+      angles[i] = 360 + angles[i]
+    angles[i] = round(angles[i], 0)
+  return angles
+
+
+def lorentz_to_poincare(embeddings, curvature=-1.0):
+  """
+  Converts lorentz coordinates to coordinates on the Poincare ball.
+  x: An array of x coordinates.
+  y: An array of y coordinates.
+  z: An array of z coordinates.
+
+  Returns the coordinates on the Poincare ball, x_p_1 and x_p_2.
+  """
+  # curvature = math.sqrt(abs(curvature))
+  # x_p_1 = y / (1 + x)
+  # x_p_2 = z / (1 + x)
+  # x_p_1 = (1/curvature * y) / (1/curvature + x)
+  # x_p_2 = (1/curvature * z) / (1/curvature + x)
+  return embeddings[:, 1:] / (1 + math.sqrt(abs(curvature)) * embeddings[:, 0:1])
+  # x = embeddings[:, 0]
+  # y = embeddings[:, 1]
+  # z = embeddings[:, 2]
+  # x_p_1 = y / (1 + math.sqrt(abs(curvature)) * x)
+  # x_p_2 = z / (1 + math.sqrt(abs(curvature)) * x)
+  # return x_p_1, x_p_2
---
+++ mt/visualization/visualization_functions.py
@@ -0,0 +1,160 @@
+import geomstats.visualization as visualization
+import math as math
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import numpy as np
+import os as os
+import scanpy as sc
+
+from matplotlib.patches import Circle
+from scipy.signal import savgol_filter
+
+from .helpers import lorentz_to_poincare
+
+
+tableau_colors = list(mcolors.TABLEAU_COLORS().keys())
+COLOR_NAMES = tableau_colors + list(mcolors.CSS4_COLORS.keys())
+
+
+def visualize_poincare_from_lorentz(embeddings,
+                                    desired_obs_all,
+                                    embedding_type="discrete",
+                                    curvature=-1.0,
+                                    cmap=plt.cm.viridis,
+                                    grid_lines=False,
+                                    origin=(0, 0),
+                                    cat_colors=None,
+                                    c_bar_label="",
+                                    bbox_to_anchor=None,
+                                    s=None):
+  poincare_coordinates = lorentz_to_poincare(embeddings, curvature)
+  fig = visualize_poincare(poincare_coordinates, desired_obs_all, curvature, embedding_type, cmap, grid_lines, cat_colors=cat_colors, c_bar_label=c_bar_label, bbox_to_anchor=bbox_to_anchor, s=s)
+  return fig
+
+def visualize_poincare(poincare_coordinates,
+                       desired_obs_all,
+                       curvature=-1.0,
+                       embedding_type="discrete",
+                       cmap=plt.cm.viridis,
+                       grid_lines=False,
+                       origin=(0, 0),
+                       cat_colors=None,
+                       c_bar_label="",
+                       bbox_to_anchor=None,
+                       s=None):
+
+  x_p_1 = poincare_coordinates[:, 0]
+  x_p_2 = poincare_coordinates[:, 1]
+  circle = visualization.PoincareDisk(coords_type="ball")
+  print(round(curvature, 1))
+  circle = Circle(origin, radius=1/math.sqrt(abs(curvature)), color='black', fill=False)
+  print(round(1/math.sqrt(abs(curvature)),1))
+  # circle.set_origin((0.5, 0.4))
+  fig, ax = plt.subplots(figsize=(8, 8))
+  ax.axes.xaxis.set_visible(grid_lines)
+  ax.axes.yaxis.set_visible(grid_lines)
+  # ax.grid(grid_lines, linestyle='--', linewidth=0.5, color='gray', alpha=0.7)
+
+  ax.set_xlim((-1/math.sqrt(abs(curvature)), 1/math.sqrt(abs(curvature))))
+  ax.set_ylim((-1/math.sqrt(abs(curvature)), 1/math.sqrt(abs(curvature))))
+
+  # circle.set_ax(ax)
+  # circle.draw(ax=ax)
+  ax.add_artist(circle)
+
+  if embedding_type == "discrete":
+    # categories = np.unique(desired_obs_all)
+    categories = desired_obs_all.cat.categories
+    print(categories)
+    if cat_colors is None:
+      cat_colors = COLOR_NAMES[0:len(categories)]
+    for i, cat in enumerate(categories):
+      cat_indices = desired_obs_all == cat
+      if s:
+        ax.scatter(x_p_1[cat_indices], x_p_2[cat_indices], label=cat, color=cat_colors[i], s=s)
+      else:
+        ax.scatter(x_p_1[cat_indices], x_p_2[cat_indices], label=cat, color=cat_colors[i])
+  else:
+    scatter = ax.scatter(x_p_1, x_p_2, c=desired_obs_all, cmap=cmap)
+    cbar = fig.colorbar(scatter, ax=ax)
+    cbar.set_label(c_bar_label)
+  if bbox_to_anchor is not None:
+    ax.legend(loc='upper right', bbox_to_anchor=bbox_to_anchor)
+  else:
+    ax.legend()
+  # ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+  # ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=2)
+  return fig
+
+def compute_umap(adata,
+                  l_neighbors,
+                  color,
+                  n_pcs=None,
+                  embeddings_key,
+                  use_original_umap=False,
+                  save_figure=False,
+                  additional_save_information=[],
+                  palette=None,
+                  title=""):
+  """
+  l_eighbors: The number of neighbors to use when creating the neighborhood map.
+  n_pcs: Dimension to use when computing neighborhood map.
+  color: A list of colors to display the UMAP in.
+  embeddings_key: The key to use to get the embeddings to use to compute the neighborhood map.
+  save_figure: True to save the figure. False otherwie.
+  additional_save_information: A list of strings to include in the save name for the figure.
+  palette: A palette specifying the color for the observition.
+  title: A title for the figure.
+  """
+  if n_pcs is None:
+      n_pcs = adata.obsm[embeddings_key].shape[1]
+  if not use_original_umap:
+    sc.pp.neighbors(adata, n_neighbors=l_neighbors, n_pcs=n_pcs, use_rep=embeddings_key)
+    sc.tl.umap(adata)
+
+  if save_figure:
+    information = [str(n_pcs),
+                      embeddings_key,
+                      str(l_neighbors),
+                    "original" if use_original_umap else "computed"]+additional_save_information
+    save = "_" + "_".join(information) + ",.png"
+  
+  sc.pl.umap(adata, color=color, save=save, palette=palette, title=title)
+
+
+def plot_gene_change(ordered_adata, phase, gene_name, colors=None, labels=None, save_path=None, ccPhase_palette=None):
+    gene_expression = ordered_adata[:, gene_name].X.squeeze(-1)
+    print(gene_expression)
+    if labels is not None:
+        categories = labels.cat.categories
+
+        for i, cat in enumerate(categories):
+            cat_indices = labels.values == cat
+
+            plt.vlines(np.where(cat_indices==True),
+                       np.zeros(np.sum(cat_indices==True)),
+                       gene_expression[cat_indices],
+                       color=colors[cat_indices],
+                       label=cat)
+    else:
+        plt.vlines(np.arange(0, len(ordered_adata)), np.zeros(len(ordered_adata)), gene_expression, colors=colors)
+
+    if ccPhase_palette:
+        color = ccPhase_palette[phase.replace("_", ".")]
+    else:
+        color = 'r'
+
+    plt.plot(savgol_filter(gene_expression,70,2), color=color)
+
+    plt.ylabel(f'${gene_name}$ expression')
+    plt.xlabel('Ordering along pseudotime')
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    plt.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.21, 1))
+    plt.tight_layout()
+    # plt.savefig(os.path.join(save_path, f'savgol_{gene_name}_{phase}_ordered_x_mb_normalize_log_italicize.png'))
+    plt.savefig(os.path.join(save_path, f'savgol_{gene_name}_{phase}_ordered_normalize_log.png'))
+    plt.show()
+    plt.close()
+    # sc.pp.pca(adata)
+    # sc.pl.pca(adata, color=['CCNE2','ccPhase'])
